12eb0484177de19113fe3f54eb341dc513b70e42
Benson Mwangi <kimaniben@gmail.com>
2024-01-29 09:28:35 +0000

    Completed assignment

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python
-
 from user import User
+class Student (User):
 
-class Student(User):
-    
-    def learn(self):
+   
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge=[]
+
+    def learn(self,n):
         pass
+        return self.knowledge.append(n)
+
+
+
+
+
+Ben =Student('Ben','Mwangi')
+Ben.learn('Physics')
+print(Ben.knowledge)
